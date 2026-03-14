591a7cd1f07f8f1f699955bffdfcc33b3c7b5183
yaslynm <152467060+yaslynm@users.noreply.github.com>
2026-03-14 13:20:41 -0500

    add headers

--- lambda/pii_detector.py
+++ lambda/pii_detector.py
@@ -1,3 +1,41 @@
+###################################################################
+#
+# detect_pii
+#
+# Detects personally identifiable information (PII) in a block
+# of text using regular expression pattern matching. The function
+# scans the text for common sensitive data types including emails,
+# phone numbers, social security numbers, and credit card numbers.
+#
+#
+# Authors:
+# Yas'lyn Mohammed
+# Reem Khalid
+# Kai'lyn Mohammed
+#
+# Parameters:
+# text : str (Input text that may contain sensitive information)
+#
+# Returns:
+# findings : list of dict (A list of dictionaries describing each detected PII instance)
+#
+#     Each dictionary contains:
+#
+#       "type"  : the type of PII detected
+#                 ("email", "phone", "ssn", "credit_card")
+#       "start" : starting index of the match in the text
+#       "end"   : ending index of the match in the text
+#
+# Example return structure:
+# [
+#   {"type": "email", "start": 12, "end": 27},
+#   {"type": "phone", "start": 36, "end": 48},
+#   {"type": "ssn", "start": 60, "end": 71},
+#   {"type": "credit_card", "start": 84, "end": 103}
+# ]
+#
+###################################################################
+
 import re
 
 EMAIL_REGEX = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b'
